24ee48310bdfabec639a95ff6106a92f07f6660a
zhiyudong <yudong.zhi@chaitin.com>
2023-05-31 15:05:01 +0800

    refactor: 重构 sql 和表;

--- demo-sql.sql
+++ demo-sql.sql
@@ -14,14 +14,12 @@ DROP TABLE IF EXISTS `blog_auth`;
 CREATE TABLE `blog_tag` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT '' COMMENT '标签名称',
-
   `created_on` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
   `modified_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
   `deleted_on` datetime DEFAULT NULL COMMENT '删除时间',
   `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
-
   `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0 为禁用、1 为启用',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='标签管理';
@@ -33,19 +31,14 @@ CREATE TABLE `blog_article` (
   `desc` varchar(255) DEFAULT '' COMMENT '文章简述',
   `cover_image_url` varchar(255) DEFAULT '' COMMENT '封面图片地址',
   `content` longtext COMMENT '文章内容',
-
-  `tag_id` int(10) unsigned NOT NULL COMMENT '标签 ID',
   `created_on` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
   `modified_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
   `deleted_on` datetime DEFAULT NULL COMMENT '删除时间',
   `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
-
   `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0 为禁用、1 为启用',
-  PRIMARY KEY (`id`),
-  KEY `tag_id` (`tag_id`),
-  CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `blog_tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章管理';
 
 
@@ -53,29 +46,22 @@ CREATE TABLE `blog_article_tag` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `article_id` int(10) unsigned NOT NULL COMMENT '文章 ID',
   `tag_id` int(10) unsigned NOT NULL COMMENT '标签 ID',
-
   `created_on` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
   `modified_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
   `deleted_on` datetime DEFAULT NULL COMMENT '删除时间',
   `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
-
-  PRIMARY KEY (`id`),
-  KEY `article_id` (`article_id`),
-  CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `blog_article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章标签关联';
-
 
 CREATE TABLE `blog_auth` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(20) DEFAULT '' COMMENT 'Key',
   `password` varchar(50) DEFAULT '' COMMENT 'Secret',
-
   `created_on` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `modified_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
   `deleted_on` datetime DEFAULT NULL COMMENT '删除时间',
   `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
-
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='认证管理';
