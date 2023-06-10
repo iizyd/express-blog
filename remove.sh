f0c72db358f3edb54a6beb28cd2efc3e12f1403d
iamzhiyudong <zhiyudong@163.com>
2023-06-11 04:36:12 +0800

    feat: 添加删除和停止的shell;

---
+++ remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 删除已存在的容器
+docker rm -f mysql-container backend-container frontend-container
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker stop mysql-container
+docker stop backend-container
+docker stop frontend-container
